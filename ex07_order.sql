025f0a38b5c7bee6509981010b9863bb1a818a73
juniel1299 <youngs5440@gmail.com>
2024-02-14 13:55:44 +0900

    0214

--- ex07_order.sql
+++ ex07_order.sql
@@ -23,3 +23,70 @@
 select * from tblInsa order by name asc;
 select * from tblInsa order by basicpay desc;
 select * from tblInsa order by jikwi asc;
+select * from tblInsa order by jikwi asc, buseo desc; --2차 정렬 
+select * from tblInsa order by jikwi asc, buseo desc, basicpay asc; -- 3차 정렬
+
+-- 비교 > 숫자, 문자, 날짜  > 정렬 가능 
+select * from tblInsa order by basicpay desc; -- 숫자
+select * from tblInsa order by name asc; --문자
+select * from tblInsa order by ibsadate desc; --날짜 
+
+-- SQL > 첨자가 1부터 시작한다. 
+select 
+    num, name, buseo, jikwi
+from tblInsa 
+order by 1 desc ; --컬럼의 순서 > 비권장 
+
+
+-- 가공된 값을 정렬 기준으로 사용 
+-- 급여순으로 정렬 
+select * from tblInsa order by basicpay desc; --basicpay
+select * from tblInsa order by basicpay + sudang desc; --basicpay + sudang
+
+
+-- 직위순으로 정렬: 부장 > 과장 > 대리 > 사원 순으로 
+select * from tblInsa order by jikwi;
+
+select
+name,jikwi,
+case
+    when jikwi = '부장' then 1
+    when jikwi = '과장' then 2
+    when jikwi = '대리' then 3
+    when jikwi = '사원' then 4
+end 
+from tblInsa
+order by 3 asc;
+
+select
+name,jikwi,
+case
+    when jikwi = '부장' then 1
+    when jikwi = '과장' then 2
+    when jikwi = '대리' then 3
+    when jikwi = '사원' then 4
+end  as jikwiseq
+from tblInsa
+order by jikwiseq asc;
+
+
+select
+name,jikwi
+from tblInsa
+order by case
+    when jikwi = '부장' then 1
+    when jikwi = '과장' then 2
+    when jikwi = '대리' then 3
+    when jikwi = '사원' then 4
+end  asc;
+
+
+-- 직원 : 남자 > 여자순으로 
+select
+name,ssn,
+ case
+    when ssn like '%-1%' then '남자'
+    when ssn like '%-2%' then '여자'
+end as gender
+from tblInsa
+order by gender asc;
---
+++ ex08_aggregation_function.sql
@@ -0,0 +1,95 @@
+-- ex08_aggregation_function.sql
+
+
+/*
+
+    함수, Function
+    1. 내장 함수(Built-in Function)
+    2. 사용자 정의 함수(User Function) > ANSI-SQL(X) , PL/SQL만 가능 
+    
+    
+    집계 함수, Aggregation function
+    -Java > count(), sum() , max(), min(),average()
+    - 아주 쉬움 > 나중에 하는 수업과 결합 > 꽤 어려워짐 
+    
+    1.count()
+    2.sum()
+    3.avg()
+    4.max()
+    5.min()
+    
+    
+    1.count()
+    - 결과 테이블의 레코드 수를 반환한다.
+    - number count(컬럼명)
+    - null 값은 제외한다 (****)
+    
+
+*/
+
+select * from tblCountry;
+
+select count(name) from tblCountry;
+select count(capital) from tblCountry;
+select count(population) from tblCountry;
+
+-- 모든 직원 수 
+select count(*) from tblInsa;
+
+-- 연락처가 있는 직원 수 
+SELECT count(*) from tblInsa where tel is not null;
+
+-- 연락처가 없는 직원 수 
+
+select count(*) from tblInsa where tel is null;
+
+-- 전체 직원 수 - 연락처가 있는 직원 수 
+select count(*) - count(tel) from tblInsa;
+
+-- tblInsa 어떤 부서들이 있나요?
+select distinct buseo from tblInsa;
+
+-- 몇개의 부서가 있나요
+select count(distinct buseo) from tblInsa;
+
+
+-- tblComedian 남자수 ? 여자수?
+
+
+select count(*) from tblComedian where gender = 'm';
+select count(*) from tblComedian where gender = 'f';
+
+-- 남자수와 여자수를 1개의 테이블로 가져오시오  *****
+
+select
+    count(case
+        when gender = 'm' then 1
+        
+    end),
+    count(case
+        when gender = 'f' then 1
+    end)
+from tblComedian;
+
+
+--tblInsa, 기획부 몇명? 총무부 몇명? 개발부 몇명?, 나머지 부서 총 몇명 ? 
+select * from tblInsa;
+
+
+select 
+    count(case
+        when buseo = '기획부' then 1
+        end),
+    count(case
+        when buseo = '총무부' then 1
+        end),
+        count(case
+            when buseo = '개발부' then 1
+            end),
+       count( case
+            when buseo not in  ('기획부','총무부','개발부')  then 1
+            end)
+       
+        from tblInsa;
+        
+        

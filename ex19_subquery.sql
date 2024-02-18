d2945fed64fcdaa1438a3a40d59118e17ca1bb7f
juniel1299 <youngs5440@gmail.com>
2024-02-19 08:08:15 +0900

    Update ex19_subquery.sql

--- ex19_subquery.sql
+++ ex19_subquery.sql
@@ -55,8 +55,8 @@ select * from tblwomen where couple= (select name from tblMen where height = 166
     1. 조건절 > 비교값으로 사용 
         a. 반환값이 1행 1열 > 단일값 반환 > 상수 취급 > 값 1개 
         b. 반환값이 N행 1열 > 다중값 반환 > 열거형 비교 > in 사용 
-        c. 반환값이 1행 N열 > 다중값 반환 > 
-        d. 반환값이 N행 N열 > 다중값 반환 
+        c. 반환값이 1행 N열 > 다중값 반환 > 그룹 비교 > N컬럼:N컬럼 
+        d. 반환값이 N행 N열 > 다중값 반환 > 2 + 3 > in + 그룹 비교 
 */
 
     -- 급여가 260만원 이상 받는 직원이 근무하는 부서의 직원 명단을 가져오시오 
@@ -66,6 +66,8 @@ select * from tblwomen where couple= (select name from tblMen where height = 166
     from tblInsa
     where buseo in ('총무부','기획부'); (select buseo from tblInsa where basicpay >=2600000 );
     
+    
+
     
     
     select
@@ -79,3 +81,110 @@ select * from tblwomen where couple= (select name from tblMen where height = 166
     where buseo in (select buseo from tblInsa where basicpay >=2600000);
     
     
+    -- '홍길동'과 같은 지역, 같은 부서인 직원 명단을 가져오시오. 
+    
+    select city, buseo from tblInsa where name = '홍길동';
+    
+    select * from tblInsa where city = '서울' and buseo = '기획부';
+    
+    
+    select * from tblInsa
+    where city = (select city from tblInsa where name = '홍길동')
+    and buseo = (select buseo from tblInsa where name = '홍길동');
+    
+    -- where 1:1 and 1:1 
+    
+    -- where 2:2 비교도 가능 . 
+    select * from tblInsa
+    where (city,buseo) = (select city,buseo from tblInsa where name = '홍길동');
+    
+    -- 급여 260만원 이상 받는 직원과 같은 부서, 같은 지역에 사는 사람 
+    
+    select * from tblInsa
+    where(buseo,city) in (select buseo,city from tblInsa where basicpay >= 2600000);
+    
+    
+    /*
+    
+       서브쿼리 삽입 위치
+    
+    1. 조건절 > 비교값으로 사용 
+        a. 반환값이 1행 1열 > 단일값 반환 > 상수 취급 > 값 1개 
+        b. 반환값이 N행 1열 > 다중값 반환 > 열거형 비교 > in 사용 
+        c. 반환값이 1행 N열 > 다중값 반환 > 그룹 비교 > N컬럼:N컬럼 
+        d. 반환값이 N행 N열 > 다중값 반환 > 2 + 3 > in + 그룹 비교 
+    
+    
+    2. 컬럼리스트 > 컬럼값으로 사용 
+        - 반드시 결과값이 1행 1열이어야 한다. > 스칼라 쿼리(원자값 반환)
+            - a. 정적 쿼리      > 모든 행에 동일한 값을 반환 
+            - b. 상관 서브 쿼리(*****)  > 메인 쿼리의 일부 컬럼값을 서브쿼리에서 사용 
+    */
+    
+        -- 집계함수를 쓸 수 있는 방법 
+    select
+    name,buseo,basicpay,
+    (select round(avg(basicpay)) from tblInsa)
+    from tblInsa;
+    
+    select
+    name,buseo,basicpay,
+    (select jikwi from tblInsa)
+    from tblInsa;
+    
+        select
+    name,buseo,basicpay,
+    (select jikwi,city from tblInsa where num = 1001)
+    from tblInsa;
+    
+    
+    
+    
+    select
+    name,buseo,basicpay,
+    (select round(avg(basicpay)) from tblInsa where buseo = a.buseo)
+    from tblInsa a;
+    
+    
+    select * from tblMen;
+    select * from tblWomen;
+    
+    -- 남자(이름,키,몸무게) + 여자 (이름 키 몸무게) 를 하나의 테이블로 가져오세요 > 결과셋
+    
+    select
+    name,height,weight,
+    couple,
+    (select name from tblWomen where name = tblmen.couple),
+    (select weight from tblWomen where name = tblmen.couple),
+    (select height from tblWomen where name = tblmen.couple)
+    from tblmen;
+    
+    
+    /*
+           서브쿼리 삽입 위치
+    
+    1. 조건절 > 비교값으로 사용 
+        a. 반환값이 1행 1열 > 단일값 반환 > 상수 취급 > 값 1개 
+        b. 반환값이 N행 1열 > 다중값 반환 > 열거형 비교 > in 사용 
+        c. 반환값이 1행 N열 > 다중값 반환 > 그룹 비교 > N컬럼:N컬럼 
+        d. 반환값이 N행 N열 > 다중값 반환 > 2 + 3 > in + 그룹 비교 
+    
+    
+    2. 컬럼리스트 > 컬럼값으로 사용 
+        - 반드시 결과값이 1행 1열이어야 한다. > 스칼라 쿼리(원자값 반환)
+            - a. 정적 쿼리      > 모든 행에 동일한 값을 반환 
+            - b. 상관 서브 쿼리(*****)  > 메인 쿼리의 일부 컬럼값을 서브쿼리에서 사용 
+    
+    3. FROM 절에서 사용 
+        - 서브 쿼리의 결과 테이블을 또 하나의 테이블이라 생각하고 메인 쿼리를 실행
+        - 인라인 뷰 (Inline View)
+    
+    */
+    
+    select * from (select name from tblInsa where name = '홍길동');      -- 테이블을 먼저 두고 이후 select
+    
+    
+    select 
+    *
+    from (select name, buseo as department, basicpay as salary from tblInsa where city = '경기')
+    where salary >= 2000000;
